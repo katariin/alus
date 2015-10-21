d617fb539a97682030f551e27beab6397d9fd33f
Jekaterina Vorošilova <jekavor@greeny.cs.tlu.ee>
2015-10-21 03:08:31 +0300

    firt commit

---
+++ vlogin.php
@@ -0,0 +1,27 @@
+<?php
+
+     // Teen errori muutujad
+	      //siiselogimine
+	$email_error = "";
+	$password_error = "";
+	      //create users
+    $name_error="";
+    $gender_error="";	
+    $create_email_error = "";
+	$create_password_error = "";		 
+	
+	//Teen vaartuse muutujad
+	$name ="";
+	$lastname="";
+	$email = "";
+	$password="";
+	$gender = "";
+	$comment="";
+	$create_email ="";
+	$create_password="";
+	
+	
+	
+	
+	
+?>
