41c14a2ea8db8c4413ef259bb40f0da482198049
Jekaterina Vorošilova <jekavor@greeny.cs.tlu.ee>
2015-11-19 00:15:07 +0200

    muutused

---
+++ funktsioon6t.php
@@ -0,0 +1,63 @@
+<?php
+
+     // yhendamine
+	    require_once("config.php");
+		$database="if15_jekavor";
+
+	// loome uue funktsiooni
+        function getClothes() {
+			
+			$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["dbname"]);
+			
+			$stmt = $mysqli->prepare("SELECT id, user_id, clothes, brand, size, color FROM fashion");
+			$stmt->bind_result($id, $user_id, $clothes, $brand, $size, $color_from_db);
+			$stmt->execute();
+			
+			//tyhi  massiv kus hoiame objekte
+			$array=array();
+			
+			
+			while($stmt->fetch()){
+				 // loon objekte
+				$mode= new StdClass();
+				$mode->id=$id;
+				$mode->user_id = $uder_id;
+				$mode->clothes = $clothes;
+				$mode->brand = $brand;
+				$mode->size = $size;
+				$mode->color = $color;
+				
+				    // lisame selle massiivi
+					array_push($array, $mode);
+				
+			
+		    }
+			
+			$stmt->close();
+			$mysqli->close();
+			
+			return $array;
+			
+		}
+		
+		 
+		
+		function deleteMode($id_to_be_deleted){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["dbname"]);
+			
+			$stmt = $mysqli->prepare("SELECT user_id, size FROM faschion WHERE id IS NULL");
+			$stmt->bind_param("i", $id_to_be_deleted);
+			
+			if($stmt->execute()){
+				header("Location: taabel.php");
+				
+			}
+			
+            $stmt->close();
+		    $mysqli->close();
+	    }
+		
+		
+		
+?>	
---
+++ taabel.php
@@ -0,0 +1,69 @@
+<?php
+
+
+          //taabel.php
+		  require_once("funktsioon6t.php");  
+		  
+		  // kas kasutaja tahab kustutada
+		  if(isset($_GET["delete"])){
+			  
+			  //saadan kaasa id, mida kustutada
+			  deleteClothes($_GET["delete"]);
+			  
+		  }
+		  
+		  
+         $clothes_list=getClothes()
+		  
+?>		  
+
+<table border=1>
+<tr>
+    <th>id</th>
+	<th>kasutaja id</th>
+	<th>Clothes</th>
+	<th>Brand</th>
+	<th>Size</th>
+	<th>Color</th>
+	<th>X</th>
+	
+   <?php
+	                   //iga massiivis olema elemendi kohta
+					   //count($clothes_list) - massiiivi pikkus
+			for($i = 0; $i < count($clothes_list); $i++){
+						   
+						if (isset($_GET["edit"]) && $clothes_list[$i]->id == $GET["edit"]{
+							  //kasutajale muutmiseks
+							 echo"<tr>";
+							   echo "<form action='taabel.php' method='post'>";
+							   echo"<form>";
+							           echo "<td>". $clothes_list[$i]->id."</td>";
+						               echo "<td>". $clothes_list[$i]->user_id."</td>";
+									   echo "<td><input name='clothes' value='" . $clothes_list[$i]->clothes."'></td>";
+									   echo "<td><input name='brand' value='" . $clothes_list[$i]->brand."'></td>";
+                                       echo "<td><input name='size' value='" . $clothes_list[$i]->size."'></td>";
+                                       echo "<td><input name='color' value='" . $clothes_list[$i]->color."'></td>";
+                                       echo "<td><input type='submit', name='update'></td>";
+                                       echo "<td><a href='taabel.php'>cancel</a></td>";									   
+							   echo"</form>";
+							   echo"<tr>Seda rida muudetakse</tr>";
+							 echo"</tr>";
+					}else{
+						   echo"<tr>";
+						   
+						   echo "<td>". $clothes_list[$i]->id."</td>";
+						   echo "<td>". $clothes_list[$i]->user_id."</td>";
+						   echo "<td>". $clothes_list[$i]->clothes."</td>";
+						   echo "<td>". $clothes_list[$i]->brand."</td>";
+						   echo "<td>". $clothes_list[$i]->size."</td>";
+						   echo "<td>". $clothes_list[$i]->color."</td>";
+						   echo "<td><a href='?delete=". $clothes_list[$i]->id."'>X</a></td>";
+						   echo "<td><a href='?edit=".$clothes_list[$i]->id."'>edit</a></td>";
+			
+				
+						   echo"</tr>";
+					}
+			}		   
+	
+   ?>
+</table>
